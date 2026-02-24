c26133a7e3dd0cabdc7f817f02b2c1a060af3aef
pankaj485 <pankajsamar2@gmail.com>
2026-02-24 16:56:20 +0530

    complete assignment 2

--- src/02-cricket-stats.js
+++ src/02-cricket-stats.js
@@ -39,20 +39,64 @@
  */
 export const calcStrikeRate = (runs, balls) => {
   // Your code here
+  if (balls <= 0 || runs < 0) {
+    return 0;
+  }
+
+  return parseFloat(((runs / balls) * 100).toFixed(2));
 };
 
 export const calcEconomy = (runsConceded, overs) => {
   // Your code here
+  if (overs <= 0 || runsConceded < 0) {
+    return 0;
+  }
+
+  return parseFloat((runsConceded / overs).toFixed(2));
 };
 
 export const calcBattingAvg = (totalRuns, innings, notOuts = 0) => {
   // Your code here
+
+  if (innings - notOuts <= 0) {
+    return 0;
+  }
+
+  return parseFloat((totalRuns / (innings - notOuts)).toFixed(2));
 };
 
 export const isAllRounder = (battingAvg, economy) => {
   // Your code here
+  return battingAvg > 30 && economy < 8;
 };
 
 export const getPlayerCard = (player) => {
   // Your code here
+  if (!player || !player.name) {
+    return null;
+  }
+
+  const {
+    name,
+    runs,
+    balls,
+    totalRuns,
+    innings,
+    notOuts,
+    runsConceded,
+    overs,
+  } = player;
+
+  const strikeRate = calcStrikeRate(runs, balls);
+  const economy = calcEconomy(runsConceded, overs);
+  const battingAvg = calcBattingAvg(totalRuns, innings, notOuts);
+  const allRounder = isAllRounder(battingAvg, economy);
+
+  return {
+    name,
+    strikeRate,
+    economy,
+    battingAvg,
+    isAllRounder: allRounder,
+  };
 };
